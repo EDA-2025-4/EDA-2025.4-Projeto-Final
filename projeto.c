4ac3acef22b67951b8e62ced4a102f642758efd9
smatheusnf <mnunes.ferreirasilva@gmail.com>
2026-02-03 13:01:42 -0300

    Prototipo (typedef struct) Adiciona lista em: Cliente e Produto

--- projeto.c
+++ projeto.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
-//#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 #define MAX_LEN_NOME 100
 #define MAX_LEN_CPF 20
 #define MAX_LEN_EMAIL 100
 #define MAX_LEN_NASC 20
 
-typedef struct cliente {
-	char nome[MAX_LEN_NOME];
-	char cpf [MAX_LEN_CPF];
-	char email [MAX_LEN_EMAIL];
-	char data_nascimento [MAX_LEN_NASC];
-}Cliente;
+typedef struct cliente Cliente;
+typedef struct produto Produto;
+typedef struct carrinho Carrinho;
 
-typedef struct produto {
-	int codigo;
-	char nome_produto [MAX_LEN_NOME];
-	float preco;
-	int quantidade;
-}Produto;
+struct cliente {
+  char nome[MAX_LEN_NOME];
+  char cpf[MAX_LEN_CPF];
+  char email[MAX_LEN_EMAIL];
+  char data_nascimento[MAX_LEN_NASC];
+
+	Carrinho *meu_carrinho;
+	Cliente *prox;
+};
+
+
+struct produto {
+  int codigo;
+  char nome_produto[MAX_LEN_NOME];
+  float preco;
+  int quantidade;
+
+	Produto *prox;
+};
