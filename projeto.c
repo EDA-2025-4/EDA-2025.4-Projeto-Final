31fc9ea13fe0bf3d8bd2c12679ea38fe12245a49
smatheusnf <mnunes.ferreirasilva@gmail.com>
2026-02-03 12:34:30 -0300

    Adiciona Definicoes e Struct (Cliente)

--- projeto.c
+++ projeto.c
@@ -2,3 +2,15 @@
 //#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+#define MAX_LEN_NOME 100
+#define MAX_LEN_CPF 20
+#define MAX_LEN_EMAIL 100
+#define MAX_LEN_NASC 20
+
+typedef struct cliente {
+	char nome[MAX_LEN_NOME];
+	char cpf [MAX_LEN_CPF];
+	char email [MAX_LEN_EMAIL];
+	char data_nascimento [MAX_LEN_NASC];
+}Cliente;
